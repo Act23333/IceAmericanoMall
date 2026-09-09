c3becf759a7d9fd4c8b4f6c599970ad1c7d288a4
Act23333 <2177667252@qq.com>
2026-09-09 22:27:23 +0800

    初步增加微信小程序登录接口

---
+++ Implementation/back-end/authorization-service/src/main/java/org/icedamericanomall/domain/dto/WechatConfigProperties.java
@@ -0,0 +1,18 @@
+package org.icedamericanomall.domain.dto;
+
+/**
+ * @ClassName: WechatConfigProperties
+ * @Description: 向微信服务接口请求
+ * @Author: noLazy
+ * @Date: 2026/9/9 21:55
+ * @Version: 1.0.0
+ * @ProjectName: back-end
+ * @Package: org.icedamericanomall.domain.dto
+ */
+public class WechatConfigProperties {
+    // 小程序ID
+    private String appId;
+
+    // 小程序密钥
+    private String appSecret;
+}
